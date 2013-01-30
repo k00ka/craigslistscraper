5113c3d12807a4e63609fb94512132c961c0d0ee
David <david@ryatta.com>
2013-01-30 01:23:06 -0500

    Refactored and commented

--- scraper.rb
+++ scraper.rb
@@ -1,47 +1,47 @@
+# Craigslist Scraper
 require 'nokogiri'
 require 'open-uri'
 
+# Craigslist uses a format for apartment details that looks like this "$XXXX Xbr - XXXXft2" where the X's are digits.
+# Using regular expressions we can pull this string apart into pieces:
+# The price is a "$" followed by digits. In regexp syntax this is /\$\d+/.
+# The number of bedrooms is a digit followed by "br". In regexp this is /\dbr/.
+# The square footage is digits follows by "ft". In regexp this is /\d+ft/.
+
+# Return a string representing the integer following the first dollar sign found in the argument
+def parse_price(details)
+  price = details.match(/\$(\d+)/)
+  return price[1] if price
+  return "price not found"
+end
+
+# Return a string representing the integer preceding the characters "br" found in the argument
+def parse_bedrooms(details)
+  # The regular expression below will match a digit followed by "br"
+  bedrooms = details.match(/(\d)br/)
+  return bedrooms[1] if bedrooms
+  return "bedrooms not found"
+end
+
+# Apartments on Craigslist are contained in a list of "items" (their term).
+# Grab the list and iterate through each item, creating a new "digest" (our term) for each apartment
+def create_digest(html)
+  apartments = []
+  items = html.css('p.row')
+  items.each do |item|
+    details = item.css('.itemph').text
+    new_digest = {
+      :description => item.css('a').text,
+      :price => parse_price(details),
+      :bedrooms => parse_bedrooms(details)
+    }
+    apartments << new_digest
+  end
+  apartments
+end
+
 craigslist_apartments_url = "http://toronto.en.craigslist.ca/apa/"
-
 craigslist_apartments = Nokogiri::HTML(open(craigslist_apartments_url))
-
-page_contents = []
-
-def parse_price(description)
-  if description.match(/\$(\d+)/)
-    price = description.match(/\$(\d+)/)
-    return price[1]
-  else
-    return 0
-  end
-end
-
-# Create a method to parse the number of bedrooms
-def parse_number_of_bedrooms(description)
-  # Craigslist uses a format for bedrooms that looks like this: 2br
-  # The regular expression below will match any digits followed by "br"
-  if description.match(/\d+br/)
-    number_of_bedrooms = description.match(/\d+br/)
-    # This will assign a string (e.g., "2br") to number_of_bedrooms
-    # We need to strip out any non-digit characters ("br"), which we can do
-    # using a second regular expression
-    return number_of_bedrooms[0].match(/\d/).to_s
-  else
-    return 0
-  end
-end
-
-craigslist_apartments.css('p.row').each do |item|
-  apartment = Hash.new
-
-  apartment["description"] = item.css('a').text
-  apartment["price"] = parse_price(item.css('.itemph').text)
-
-  # Let's now add the number of bedrooms to our hash
-  apartment["number_of_bedrooms"] = parse_number_of_bedrooms(item.css('.itemph').text)
-
-  page_contents << apartment
-
-end
-
-puts page_contents
+apartments = create_digest(craigslist_apartments)
+# Output our new list of apartments unless we're running tests, then don't
+puts apartments unless defined? MiniTest::Spec
